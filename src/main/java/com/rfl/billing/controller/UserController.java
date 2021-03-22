9b61404add17bdb949e9acd9b7ce82c7207b9a9a
dikart <dikartone@gmail.com>
2021-03-22 23:34:41 +0300

    work-to-home commit

--- src/main/java/com/rfl/billing/controller/UserController.java
+++ src/main/java/com/rfl/billing/controller/UserController.java
@@ -1,12 +1,27 @@
 package com.rfl.billing.controller;
 
+import com.rfl.billing.model.User;
+import com.rfl.billing.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
-public class UserController {
+public class UserController extends AbstractUserController{
+
+    private PasswordEncoder encoder;
+
+    @Autowired
+    public UserController(PasswordEncoder encoder) {
+        this.encoder = encoder;
+    }
+
     @GetMapping
     public String home() {
         return "index";
@@ -17,8 +32,9 @@ public class UserController {
         return "login";
     }
 
-    @GetMapping("register")
-    public String getRegisterForm() {
-        return "register";
+    @PostMapping("register")
+    public String register(@ModelAttribute User user, Model model) {
+        service.create(user);
+        return "index";
     }
 }
